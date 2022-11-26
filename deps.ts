ecb47eb063e696c72f0ebd82ba9467a5b808c828
m19e <nexus.me7290@gmail.com>
2022-11-26 23:04:48 +0900

    Migrate from old project

--- deps.ts
+++ deps.ts
@@ -1,1 +1,6 @@
 // export { parse } from "https://deno.land/std@0.115.1/flags/mod.ts";
+export {
+  createSchema,
+  createYoga,
+} from "https://cdn.skypack.dev/graphql-yoga?dts";
+export { serve } from "https://deno.land/std@0.157.0/http/server.ts";
