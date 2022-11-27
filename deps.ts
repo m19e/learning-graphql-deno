f6e49179b522f7015c90886a33f949c29f574930
m19e <nexus.me7290@gmail.com>
2022-11-27 21:47:38 +0900

    Remove commented module

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
-// export { parse } from "https://deno.land/std@0.115.1/flags/mod.ts";
 export { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 export { createSchema, createYoga } from "https://cdn.skypack.dev/graphql-yoga";
 export { GraphQLScalarType } from "https://cdn.skypack.dev/graphql";
