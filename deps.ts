9eadf7a6676d3330dc91d929e12199f59249b9c4
m19e <nexus.me7290@gmail.com>
2022-11-29 21:16:24 +0900

    Add modules for oak middleware

--- deps.ts
+++ deps.ts
@@ -1,7 +1,13 @@
 export { Hono } from "https://deno.land/x/hono/mod.ts";
 export { graphqlServer } from "npm:@honojs/graphql-server";
 export { buildSchema } from "https://cdn.skypack.dev/graphql";
-
 export { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 export { createSchema, createYoga } from "https://cdn.skypack.dev/graphql-yoga";
+
+export { Application, Router } from "https://deno.land/x/oak@v10.0.0/mod.ts";
+export {
+  applyGraphQL,
+  gql,
+  GQLError,
+} from "https://deno.land/x/oak_graphql/mod.ts";
 export { GraphQLScalarType } from "https://cdn.skypack.dev/graphql";
