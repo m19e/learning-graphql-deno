58fd9cd793e30e3d3130d087eb74651eea573226
m19e <nexus.me7290@gmail.com>
2022-11-29 19:58:40 +0900

    Add pkgs for hono middleware

--- deps.ts
+++ deps.ts
@@ -1,3 +1,7 @@
+export { Hono } from "https://deno.land/x/hono/mod.ts";
+export { graphqlServer } from "npm:@honojs/graphql-server";
+export { buildSchema } from "https://cdn.skypack.dev/graphql";
+
 export { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 export { createSchema, createYoga } from "https://cdn.skypack.dev/graphql-yoga";
 export { GraphQLScalarType } from "https://cdn.skypack.dev/graphql";
