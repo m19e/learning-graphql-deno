417a5491a8480affa682d74f6994315474a29c3b
m19e <nexus.me7290@gmail.com>
2022-12-07 23:42:14 +0900

    Add comment for create photos process

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,3 +1,20 @@
+-- create type public.category as enum (
+--   'SELFIE',
+--   'PORTRAIT',
+--   'ACTION',
+--   'LANDSCAPE',
+--   'GRAPHIC'
+-- );
+
+-- create table public.photos (
+--     id integer primary key generated always as identity,
+--     url text not null,
+--     name text not null,
+--     description text,
+--     category category
+--     created date
+-- );
+
 insert into public.users (name, github_login)
 values
   ('Rio Tsukatsuki', 'rTsukatsuki'),
