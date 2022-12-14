cb2bb809db28b3a41bd9e73f6c6687e8810ac258
m19e <nexus.me7290@gmail.com>
2022-12-14 14:53:54 +0900

    Add users in seed for debug

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -25,4 +25,7 @@ insert into public.photos (name, description, category, github_user, created)
 values
   ('photo/1 name', 'photo/1 description', 'PORTRAIT', 'hAkeboshi', '11-23-2032'),
   ('photo/2 name', 'photo/2 description', 'SELFIE', 'nUshio', '11-25-2032'),
-  ('photo/3 name', 'photo/3 description', 'PORTRAIT', 'yHayase', '11-26-2032')
+  ('photo/3 name', 'photo/3 description', 'PORTRAIT', 'yHayase', '11-26-2032'),
+  ('photo/4 name', 'photo/4 description', 'PORTRAIT', 'm19e', '12-11-2032'),
+  ('photo/5 name', 'photo/5 description', 'PORTRAIT', 'm19e', '12-12-2032'),
+  ('photo/6 name', 'photo/6 description', 'PORTRAIT', 'm19e', '12-13-2032')
