ef22dbb55f9786217ce85d0e8bb7303312816f0c
m19e <nexus.me7290@gmail.com>
2022-12-15 20:31:05 +0900

    Add find-user-by-login query

--- lib/query.ts
+++ lib/query.ts
@@ -48,3 +48,22 @@ export const findUserByToken = async (
   const [userEdge] = data.usersCollection.edges;
   return convertRecordToUser(userEdge.node);
 };
+
+export const findUserByLogin = async (login: string): Promise<User | null> => {
+  const { data, errors } = await postWithHeaders<FindUserResponse>({
+    query: findUserQuery,
+    variables: {
+      filter: {
+        github_login: {
+          eq: login,
+        },
+      },
+    },
+  });
+  if (errors) {
+    console.error(errors);
+    return null;
+  }
+  const [edge] = data.usersCollection.edges;
+  return convertRecordToUser(edge.node);
+};
