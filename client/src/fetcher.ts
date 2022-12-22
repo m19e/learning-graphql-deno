0bd071e21f01ec28f0a36465e04db7a98fa008ef
m19e <nexus.me7290@gmail.com>
2022-12-22 20:52:08 +0900

    Add github-auth fetcher

--- client/src/fetcher.ts
+++ client/src/fetcher.ts
@@ -2,17 +2,21 @@ import ky from "ky";
 import { QueryClient } from "react-query";
 
 import type { Options, Response } from "./types";
-import { ADD_FAKE_USERS_MUTATION, ALL_USERS_QUERY } from "./query";
+import {
+  ADD_FAKE_USERS_MUTATION,
+  ALL_USERS_QUERY,
+  GITHUB_AUTH_MUTATION,
+} from "./query";
 import {
   AddFakeUsersMutation,
   AddFakeUsersMutationVariables,
   AllUsersQuery as AllUsersData,
+  GithubAuthMutation,
+  GithubAuthMutationVariables,
 } from "./generated";
 
 const endpoint = "http://localhost:4000/graphql";
 const fetcher = async <T>(options: Options): Promise<T | undefined> => {
-  console.log("fetch now: ", new Date());
-
   const res = await ky
     .post(endpoint, { json: options })
     .json<Response<T>>();
@@ -28,6 +32,15 @@ export const fetchAddFakeUsers = async (
 ) => {
   return await fetcher<AddFakeUsersMutation>({
     query: ADD_FAKE_USERS_MUTATION,
+    variables,
+  });
+};
+
+export const fetchGithubAuth = async (
+  variables: GithubAuthMutationVariables,
+) => {
+  return await fetcher<GithubAuthMutation>({
+    query: GITHUB_AUTH_MUTATION,
     variables,
   });
 };
