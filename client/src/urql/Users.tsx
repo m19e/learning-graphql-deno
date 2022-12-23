a76359f193b072dd651271a8036180a78b1a1c49
m19e <nexus.me7290@gmail.com>
2022-12-23 22:57:25 +0900

    Display user list

--- client/src/urql/Users.tsx
+++ client/src/urql/Users.tsx
@@ -5,9 +5,11 @@ import { Exact, UsersComponent, UsersQuery } from "../gql";
 export const Users = () => {
   return (
     <Suspense fallback={<UsersFallback />}>
-      <UsersComponent>
-        {(props) => <UsersContent {...props} />}
-      </UsersComponent>
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <UsersComponent>
+          {(props) => <UsersContent {...props} />}
+        </UsersComponent>
+      </div>
     </Suspense>
   );
 };
@@ -28,9 +30,30 @@ type UsersProps = QueryState<
 >;
 
 const UsersContent = ({ data }: UsersProps) => {
+  if (!data) return null;
+  const { totalUsers, allUsers } = data;
   return (
-    <p className="whitespace-pre text-left p-2 bg-gray-200">
-      {JSON.stringify(data, null, 4)}
-    </p>
+    <div className="flex flex-col items-start gap-4 bg-gray-100 p-4">
+      <span className="text-teal-500 font-semibold">{totalUsers} Users</span>
+      <div className="inline-flex gap-2">
+        <button className="bg-blue-700 hover:bg-blue-600 text-white rounded px-4 py-2">
+          Refetch User
+        </button>
+        <button className="bg-green-700 hover:bg-green-600 text-white rounded px-4 py-2">
+          Add Fake Users
+        </button>
+      </div>
+      {allUsers.map((user) => (
+        <div key={user.githubLogin} className="flex items-center gap-2">
+          <img
+            src="https://placehold.jp/48x48.png"
+            alt="placehold"
+          />
+          <span className="text-blue-600 text-lg">
+            {user.name ?? user.githubLogin}
+          </span>
+        </div>
+      ))}
+    </div>
   );
 };
