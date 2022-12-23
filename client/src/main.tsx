fd2b71b301c574965e4817f519b4ac4510bf2137
m19e <nexus.me7290@gmail.com>
2022-12-23 20:10:42 +0900

    Display urql app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { RQApp } from "./react-query/App";
+import { UrqlApp } from "./urql/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RQApp />
+    <UrqlApp />
+    {/* <RQApp /> */}
   </React.StrictMode>,
 );
