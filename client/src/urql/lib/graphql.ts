98db1720031dcc9086ee3c1d73411e41859e30bf
m19e <nexus.me7290@gmail.com>
2022-12-24 17:35:14 +0900

    Set token via urql fetch-options

--- client/src/urql/lib/graphql.ts
+++ client/src/urql/lib/graphql.ts
@@ -2,6 +2,14 @@ import { createClient } from "urql";
 
 export const client = createClient({
   url: "http://localhost:4000/graphql",
+  fetchOptions: () => {
+    const token = import.meta.env.VITE_TOKEN;
+    return {
+      headers: {
+        "authorization": token,
+      },
+    };
+  },
   suspense: true,
   maskTypename: true,
 });
