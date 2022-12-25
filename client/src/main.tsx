56ca080e9befcba19f413f551bd18311b2e607dc
m19e <nexus.me7290@gmail.com>
2022-12-25 16:08:07 +0900

    Remove react-query app from main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { RQApp } from "./react-query/App";
 import { UrqlApp } from "./urql/App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <UrqlApp />
-    {/* <RQApp /> */}
   </React.StrictMode>,
 );
