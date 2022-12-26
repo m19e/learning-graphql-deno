e9564fe82d9b590e968a2ea3dafda40b4b9c82d4
m19e <nexus.me7290@gmail.com>
2022-12-26 19:37:31 +0900

    Get authed token from local-storage

--- client/src/urql/lib/graphql.ts
+++ client/src/urql/lib/graphql.ts
@@ -3,10 +3,10 @@ import { createClient } from "urql";
 export const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: () => {
-    const token = import.meta.env.VITE_TOKEN;
+    // const token = import.meta.env.VITE_TOKEN;
     return {
       headers: {
-        "authorization": token,
+        authorization: localStorage.getItem("token")!,
       },
     };
   },
