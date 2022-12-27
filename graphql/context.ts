10e36d9a07ddd3f2d7d6eb02f0de1cd436ed18c1
m19e <nexus.me7290@gmail.com>
2022-12-27 14:58:53 +0900

    Nullable token

--- graphql/context.ts
+++ graphql/context.ts
@@ -4,7 +4,7 @@ import { findUserByToken } from "../lib/query.ts";
 type ContextProps = {
   request: {
     headers: {
-      get: (key: string) => string;
+      get: (key: string) => string | null;
     };
   };
 };
@@ -12,6 +12,7 @@ type Context = (ctx: ContextProps) => Promise<Ctx>;
 
 export const context: Context = async ({ request }) => {
   const githubToken = request.headers.get("authorization");
+  if (githubToken === null) return { currentUser: null };
   const currentUser = await findUserByToken(githubToken);
   return {
     currentUser,
