b97abccd63b7608e49a307086100358df5b7814c
m19e <nexus.me7290@gmail.com>
2022-12-31 13:45:46 +0900

    Remove commented

--- server.ts
+++ server.ts
@@ -7,18 +7,6 @@ const typeDefs = await Deno.readTextFile("./graphql/typeDefs.graphql");
 
 const app = new Application();
 
-// app.use(async (ctx, next) => {
-//   await next();
-//   const rt = ctx.response.headers.get("X-Response-Time");
-//   console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-// });
-// app.use(async (ctx, next) => {
-//   const start = Date.now();
-//   await next();
-//   const ms = Date.now() - start;
-//   ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-// });
-
 const router = new Router().get(
   "/",
   ({ response }) => response.body = "Welcome to the PhotoShare API",
